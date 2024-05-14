48bdd8fc3c2dced3f06b9f5401c86855ce0d9278
OmarAlnahhas <76499086+OmarAlnahhas@users.noreply.github.com>
2024-05-14 06:37:24 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,23 @@
+# COLORS (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1100
+HEIGHT = 641
+FPS = 60
+title = "Tile-Based game"
+BGCOLOUR = DARKGREY
+TILESIZE = 128
+
+EASY = 3 * TILESIZE
+MEDIUM = 4 * TILESIZE
+HARD = 5 * TILESIZE
+
+
